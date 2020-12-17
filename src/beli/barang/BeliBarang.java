939d9a1d92503b69ee84adca43a82f8bebd6ccef
unknown <aldyaliandi@gmail.com>
2020-12-17 10:12:57 +0700

    Tugas Netbeans

---
+++ src/beli/barang/BeliBarang.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beli.barang;
+
+/**
+ *
+ * @author Aldy
+ */
+public class BeliBarang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        barangdibeli kasep = new barangdibeli();
+        kasep.setVisible(true);
+    }
+    
+}
